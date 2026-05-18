2ff981c370732f42a8bd6cba5e39ba5f429173ad
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-18 15:59:06 -0500

    Update MoneyManager.cs

--- Financial/MoneyManager.cs
+++ Financial/MoneyManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Xml.Linq;
@@ -7,10 +7,10 @@ using XmlUtilities;
 
 namespace Financial
 {
-	// Token: 0x02000007 RID: 7
+	// Token: 0x02000008 RID: 8
 	public static class MoneyManager
 	{
-		// Token: 0x06000012 RID: 18 RVA: 0x00002708 File Offset: 0x00000908
+		// Token: 0x0600001D RID: 29 RVA: 0x00002F38 File Offset: 0x00001138
 		public static void Initialize()
 		{
 			foreach (XElement xelement in ContentManager.Get<XElement>("Financial", null).Descendants("Items"))
@@ -25,19 +25,19 @@ namespace Financial
 			}
 		}
 
-		// Token: 0x06000013 RID: 19 RVA: 0x000027C4 File Offset: 0x000009C4
+		// Token: 0x0600001E RID: 30 RVA: 0x00002FF0 File Offset: 0x000011F0
 		public static int DecodeValue(string result)
 		{
 			string[] array = result.Split(new char[]
 			{
 				':'
 			});
-            Block block = BlocksManager.GetBlock(array[0]);
-            int num = (array.Length >= 2) ? int.Parse(array[1], CultureInfo.InvariantCulture) : 0;
-			return Terrain.MakeBlockValue(block.BlockIndex, 0, num);
+			Block block = BlocksManager.FindBlockByTypeName(array[0], true);
+			int data = (array.Length >= 2) ? int.Parse(array[1], CultureInfo.InvariantCulture) : 0;
+			return Terrain.MakeBlockValue(block.BlockIndex, 0, data);
 		}
 
-		// Token: 0x04000012 RID: 18
+		// Token: 0x04000027 RID: 39
 		public static List<Money> m_moneys = new List<Money>();
 	}
 }
