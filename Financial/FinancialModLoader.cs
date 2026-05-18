08a3526d3004026eecc555a9b2a05b9ad2e72b17
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-05-18 16:21:16 -0500

    Update FinancialModLoader.cs

--- Financial/FinancialModLoader.cs
+++ Financial/FinancialModLoader.cs
@@ -1,43 +1,27 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using Engine;
 using Game;
 
 namespace Financial
 {
+	// Token: 0x02000009 RID: 9
 	public class FinancialModLoader : ModLoader
 	{
+		// Token: 0x06000020 RID: 32 RVA: 0x00003047 File Offset: 0x00001247
 		public override void __ModInitialize()
 		{
 			ModsManager.RegisterHook("OnLoadingFinished", this);
 		}
 
+		// Token: 0x06000021 RID: 33 RVA: 0x00003054 File Offset: 0x00001254
 		public override void OnLoadingFinished(List<Action> actions)
 		{
-			actions.Add(delegate ()
+			actions.Add(delegate
 			{
-				LoadLanguage();
 				MoneyManager.Initialize();
 				Log.Information(LanguageControl.Get("FinancialModLoader", 1));
 			});
-		}
-
-		private void LoadLanguage()
-		{
-			try
-			{
-				Entity.GetAssetsFile("Financial.json", delegate (Stream stream)
-				{
-					if (stream != null)
-					{
-						LanguageControl.loadJson(stream);
-					}
-				});
-			}
-			catch
-			{
-			}
 		}
 	}
 }
